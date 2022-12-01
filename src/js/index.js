e0c7c84c5710bcf3e245b37aaf7cae16f9f89666
Santiago Pintus <santiagopintus@gmail.com>
2022-12-01 18:02:40 -0300

    Added a nice animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-// document.getElementById("output").innerText = 'Sucessfully setup!';
+import { customFetch } from "./utils";
+import { manageStar } from "./utils";
+
+const starWikiApp = () => {
+  manageStar();
+  const apiUrl = "https://akabab.github.io/starwars-api/api/all.json";
+  getCharacters(apiUrl);
+};
+
+const getCharacters = (url) => {
+  customFetch(url).then(data => console.log(data))
+};
+
+const showCharacters = (char) => {
+  console.log(char);
+};
+
+window.onload = starWikiApp;
